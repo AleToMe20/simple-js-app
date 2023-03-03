34ee4c02d719f0a99276589b9b83b43175ad9b35
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-03-03 11:24:19 +0100

    Update scripts.js
    set let for favoritefood

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-let myName = 'Bob';
-document.write(myName)
+// push notification
+alert('Hello world');
+
+// assigning favoriteFood as pasta
+let favoriteFood = 'Pasta';
+document.write(favoriteFood);
