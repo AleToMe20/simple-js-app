ab9542c6ded70b2bebbae829f6ddb5ee259b0f7d
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-03-04 18:24:26 +0100

    Update scripts.js
    Added two more objects to the poemonList Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-
 // assigning three pokemon Objects to pokemonList array 
-let pokemonList = [
-// defining three pokemon objects
-{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+let pokemonList = [{
+
+    // defining three pokemon objects
+    name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'],
+    name: 'Ivysaur', height: 1, types: ['grass', 'poison'],
+    name: 'Venusaur', height: 2, types: ['grass', 'poison'],
+},
 ];
