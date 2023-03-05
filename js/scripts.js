a4469c131be67c3aec138d4cfb24ed965c86b455
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-03-05 20:41:25 +0100

    Update scripts.js
    incl. for Loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 // assigning three pokemon Objects to pokemonList array 
 let pokemonList = [
-
     // defining three pokemon objects
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-    {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
-,
-];
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison'
+        ]
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        types: ['grass', 'poison']
+    }
+]
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) {
+        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")  - Wow, that's big!");
+    } 
+    else {
+        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height);
+    }
+}
