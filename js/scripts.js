9707667d6ffdc818b9a303e2022bd798b71dcf29
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-03-06 10:29:17 +0100

    Update scripts.js
    Deleted the document.write 'duplicate'

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,9 @@ let pokemonList = [
     }
 ]
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
     document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+
     if (pokemonList[i].height >= 1.5) {
-        document.write("Wow, that's big!!")
         document.write(" Wow, that's big!!") + "</p>"
     }
 }
