b48fe6e5ad346c4ef218f8e0326650c43aefba75
AleToMe20 <78073236+AleToMe20@users.noreply.github.com>
2023-03-08 23:13:23 +0100

    Code containing IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,42 @@
 // assigning three pokemon Objects to pokemonList array 
-let pokemonList = [
-    // defining three pokemon objects
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison'
-        ]
-    },
-    {
-        name: 'Ivysaur',
-        height: 1,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Venusaur',
-        height: 2,
-        types: ['grass', 'poison']
-    }
-]
+let pokemonRepository = (function () {
+    let pokemonList = [// empty array
+        // defining three pokemon objects
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison'
+            ]
+        },
+        {
+            name: 'Ivysaur',
+            height: 1,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Venusaur',
+            height: 2,
+            types: ['grass', 'poison']
+        }
+    ];
+    return {
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        },
 
-    pokemonList.forEach(function(pokemon){
+        getAll: function () {
+            return pokemonList;
+        }
+    };
+    pokemonList.forEach(function (pokemon) {
         if (pokemonList.height >= 1.5) {
             document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a big Pokemon!!" + "<p>")
         } else {
             document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a small Pokemon!!" + "<p>")
         }
-    });
+    }
+    );
+})();
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name + pokemon.height);
+});
